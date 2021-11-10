21ed7dbb41008d56718cbbe91ecc6aa23f3c112c
Кирилл Богомолов <kirill.bogomolov.95@yandex.ru>
2021-11-10 13:12:21 +0300

    homework 2

---
+++ task2/solution/cmake-build-debug/CMakeFiles/solution.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solution.dir/src/main_src.c.o"
+  "CMakeFiles/solution.dir/src/pipe_src.c.o"
+  "CMakeFiles/solution.dir/src/string_src.c.o"
+  "solution"
+  "solution.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/solution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
