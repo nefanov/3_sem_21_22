e79163c5b1154da60533b1660b8717ae422556bc
Кирилл Богомолов <kirill.bogomolov.95@yandex.ru>
2021-11-12 20:51:31 +0300

    task 4

--- task4/integral/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ task4/integral/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -50,5 +50,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/solution.dir/DependInfo.cmake"
+  "CMakeFiles/integrate.dir/DependInfo.cmake"
   )
---
+++ task4/integral/cmake-build-debug/CMakeFiles/integrate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/integrate.dir/src/main.c.o"
+  "integrate"
+  "integrate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/integrate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
